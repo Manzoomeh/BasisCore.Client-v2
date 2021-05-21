f41c48146a728df274d5a02263656b58aea4000d
Qamsari <44198226+Qamsari@users.noreply.github.com>
2021-05-21 07:33:03 +0430

    Edit tryParse in BooleanToken classes

--- src/token/boolean/BooleanArray.ts
+++ src/token/boolean/BooleanArray.ts
@@ -1,9 +1,11 @@
 ﻿import IToken from "../IToken";
 import ArrayToken from "../base/ArrayToken";
+import BooleanObject from "./BooleanObject";
 
 export default class BooleanArray extends ArrayToken<boolean> {
   tryParse(value: string): boolean {
-    return (value ?? "false").toLowerCase() == "true";
+    return BooleanObject.tryParse(value);
+    //return (value ?? "false").toLowerCase() == "true";
   }
   constructor(...collection: Array<IToken<string>>) {
     super(...collection);
--- src/token/boolean/BooleanObject.ts
+++ src/token/boolean/BooleanObject.ts
@@ -4,7 +4,12 @@ export default class BooleanObject extends ObjectToken<boolean> {
   constructor(rawValue: string) {
     super(rawValue);
   }
+
+  public static tryParse(value: string) {
+    return value == null ? null : value.toLowerCase() === "true";
+  }
   tryParse(value: string): boolean {
-    return (value ?? "false").toLowerCase() == "true";
+    return BooleanObject.tryParse(value);
+    //return  (value ?? "false").toLowerCase() == "true";
   }
 }
