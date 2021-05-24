e4226d5254a006c6c1cf8cf98f97172cf2c7a720
Qamsari <44198226+Qamsari@users.noreply.github.com>
2021-05-24 17:07:41 +0430

    GroupComponent fix bug

--- src/component/collection/GroupComponent.ts
+++ src/component/collection/GroupComponent.ts
@@ -1,18 +1,29 @@
 import { injectable, inject } from "tsyringe";
 import IContext from "../../context/IContext";
+import Component from "../Component";
 import { NonSourceBaseComponent } from "../NonSourceBaseComponent";
 import ComponentCollection from "./ComponentCollection";
 
 @injectable()
-export default class GroupComponent extends NonSourceBaseComponent {
+export default class GroupComponent extends Component<Element> {
   private collection: ComponentCollection;
   private observer: MutationObserver;
+  readonly range: Range;
+  readonly content: DocumentFragment;
   protected runAsync(): Promise<void> {
     return this.collection.runAsync();
   }
   constructor(element: Element, @inject("IContext") context: IContext) {
     super(element, context);
-    console.log("gg", this.range, this.content);
+
+    this.content = document.createDocumentFragment();
+    while (element.hasChildNodes()) {
+      this.content.appendChild(element.firstChild);
+    }
+    this.range = document.createRange();
+    this.range.selectNode(element);
+
+    console.log("gg", this.content);
     const config = { attributes: true, childList: true, subtree: true };
     this.observer = new MutationObserver(() => {
       console.log("t");
@@ -20,7 +31,7 @@ export default class GroupComponent extends NonSourceBaseComponent {
     });
     this.observer.observe(this.content, config);
     this.collection = new ComponentCollection(
-      Array.from(this.content.firstChild.childNodes),
+      Array.from(this.content.childNodes),
       this.context
     );
 
@@ -31,3 +42,31 @@ export default class GroupComponent extends NonSourceBaseComponent {
     this.range.insertNode(content.cloneNode(true));
   }
 }
+
+// export default class GroupComponent extends NonSourceBaseComponent {
+//   private collection: ComponentCollection;
+//   private observer: MutationObserver;
+//   protected runAsync(): Promise<void> {
+//     return this.collection.runAsync();
+//   }
+//   constructor(element: Element, @inject("IContext") context: IContext) {
+//     super(element, context);
+//     console.log("gg", this.content.firstChild);
+//     const config = { attributes: true, childList: true, subtree: true };
+//     this.observer = new MutationObserver(() => {
+//       console.log("t");
+//       this.setContentEx(this.content);
+//     });
+//     this.observer.observe(this.content.firstChild, config);
+//     this.collection = new ComponentCollection(
+//       Array.from(this.content.firstChild.childNodes),
+//       this.context
+//     );
+
+//     //this.collection.runAsync().then((x) => this.setContentEx(this.content));
+//   }
+//   private async setContentEx(content: DocumentFragment) {
+//     this.range.deleteContents();
+//     this.range.insertNode(content.firstChild.cloneNode(true));
+//   }
+// }
