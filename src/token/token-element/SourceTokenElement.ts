7ea94cccb7e6723665122fae17f8b4818fc2b027
Qamsari <44198226+Qamsari@users.noreply.github.com>
2021-06-07 11:08:03 +0430

    Use Function in SourceTokenElement Edit ObjectToken to use SourceTokenElement.extractValue()

--- src/token/token-element/SourceTokenElement.ts
+++ src/token/token-element/SourceTokenElement.ts
@@ -1,17 +1,42 @@
-﻿import ITokenElement from "./ITokenElement";
+﻿import ISource from "../../data/ISource";
+import ITokenElement from "./ITokenElement";
 
 export default class SourceTokenElement implements ITokenElement {
   source: string;
   member?: string;
   column?: string;
+  readonly extractValue: (source: ISource) => string;
 
   constructor(parts: Array<string>) {
     this.source = parts[0];
     this.member = parts[1];
     this.column = parts.length > 2 ? parts.slice(2).join(".") : null;
+    this.extractValue = new Function(
+      "source",
+      `let retVal = null;
+      if (source.data.rows.length == 1) {
+        retVal = (source.data.rows[0].${this.column})?.toString();
+      } else if (source.data.rows.length > 1) {
+        retVal = source.data.rows
+              .map((row) => row.${this.column})
+              .join(",");
+      }
+      return retVal;`
+    ) as any;
   }
 
   get sourceName(): string {
     return `${this.source}.${this.member}`;
   }
+
+  // g(source: ISource): any {
+  //   let retVal = null;
+  //   if (source.data.rows.length == 1) {
+  //     retVal = source.data.rows[0];
+  //   } else if (source.data.rows.length > 1) {
+  //     retVal = source.data.rows
+  //     .filter((row) => row).join(",");
+  //   }
+  //   return retVal;
+  // }
 }
