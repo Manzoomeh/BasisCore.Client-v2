a74d8088d8dd5b08bc40978fcb8610d256246361
Zahra Akbari <akbari.z1002@gmail.com>
2024-07-30 10:12:12 +0330

    edit version

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,7 @@ ______           _                               _ _            _
                                                                     
 %cWelcome To BasisCore Ecosystem%c
 follow us on https://BasisCore.com/
-version:2.33.0`,
+version:2.33.1`,
   " background: yellow;color: #0078C1; font-size: 2rem; font-family: Arial; font-weight: bolder",
   "color: #0078C1; font-size: 1rem; font-family: Arial;"
 );
