3a7a815a1ee98833870d622b882a3c7186f85f48
Qamsari <44198226+Qamsari@users.noreply.github.com>
2025-02-15 09:37:23 +0330

    add callback

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,7 @@ ______           _                               _ _            _
 follow us on https://BasisCore.com/
 
 
-version:2.38.6`,
+version:2.39.0`,
 
   " background: yellow;color: #0078C1; font-size: 2rem; font-family: Arial; font-weight: bolder",
   "color: #0078C1; font-size: 1rem; font-family: Arial;"
