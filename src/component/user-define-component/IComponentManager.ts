362000597a319741df846b0d28de40eb40737773
Qamsari <44198226+Qamsari@users.noreply.github.com>
2025-05-03 18:05:10 +0330

    add disposeAsync() to IComponentManager and related business #83

--- src/component/user-define-component/IComponentManager.ts
+++ src/component/user-define-component/IComponentManager.ts
@@ -3,4 +3,5 @@ import ISource from "../../data/ISource";
 export default interface IComponentManager {
   initializeAsync(): Promise<void>;
   runAsync(source?: ISource): Promise<any>;
+  disposeAsync(): Promise<void>;
 }
